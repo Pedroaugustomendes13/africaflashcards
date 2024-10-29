063e40e70d09b7cb6c45e806c7654d1c1d53a3c2
Pedroaugustomendes13 <140609063+Pedroaugustomendes13@users.noreply.github.com>
2024-10-29 08:30:37 -0300

    dsfsdf

---
+++ pergunta.js
@@ -0,0 +1,65 @@
+criaCartao(
+    'Matemática',
+    '5x5',
+    '25'
+)
+criaCartao(
+    'Educação Financeira',
+    'Qual o preço do dólar em reais?',
+    'R$5,50.'
+)
+criaCartao(
+    'Português',
+    'Qual é o correto, mêses ou meses?',
+    'Meses.'
+)
+criaCartao(
+    'História',
+    'Em que ano foi descoberto o Brasil?',
+    '1500.'
+)
+criaCartao(
+    'Inglês',
+    'Transforme a frase em uma pergunta: you know.',
+    'Do you know?'
+)
+criaCartao(
+    'Geografia',
+    'Qual o estado localizado em cima de Santa Catarina?',
+    'Paraná.'
+)
+criaCartao(
+    'Sociologia',
+    'O que é extratificação social?',
+    'Divisão da sociedade em camadas.'
+)
+criaCartao(
+    'Química',
+    'O que é Ph?',
+    'Medida para medir a acidez de uma solução.'
+)
+criaCartao(
+    'Educação financeira',
+    'Qual o preço do dólar em reais?',
+    'R$5,51.'
+)
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'É um nome para guardar uma informação.'
+)
+criaCartao(
+    'Sociologia',
+    'O que é extratificação social?',
+    'Divisão da sociedade em camadas.'
+)
+criaCartao(
+    'Projeto de vida',
+    'O que é um projeto de vida?',
+    'É um plano para alcançar suas metas.'
+)
+criaCartao(
+    'Extra',
+    'O que é? O que é? Vários pontinhos amarelos na parede?',
+    'Fandangos alpinistas.'
+)
